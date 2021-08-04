2a30b72132ea66a9405fd0f92172bafd32789e1a
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-08-04 14:19:58 -0300

    Método getVersion

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getVersion(): unknown {
+    return {
+      name: 'UPA Helper',
+      version: '1.0.0',
+    };
   }
 }
