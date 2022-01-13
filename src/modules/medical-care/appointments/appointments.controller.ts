4f1beb30269d3c46efb1c87bfbb30c0e5d6cc930
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-01-13 14:44:19 -0300

    Salvar essa Consulta

--- src/modules/medical-care/appointments/appointments.controller.ts
+++ src/modules/medical-care/appointments/appointments.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Appointment } from './appointment.entity';
+import { AppointmentsService } from './appointments.service';
+import { AppointmentDto } from './dto/appointment.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('appointments')
-export class AppointmentsController {}
+@ApiTags('Appointment')
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Cadastra uma Consulta' })
+  async createAppointment(
+    @Body() appointment: AppointmentDto,
+  ): Promise<Appointment> {
+    return await this.appointmentsService.createAppointment(appointment);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Retorna uma Consulta especifica' })
+  async getAppointmentById(@Param('id') id: number): Promise<Appointment> {
+    const appointment = await this.appointmentsService.getAppointmentById(id);
+
+    if (!appointment) {
+      throw new NotFoundException('Essa consulta não existe');
+    }
+
+    return appointment;
+  }
+}
--- src/modules/medical-care/appointments/appointments.service.ts
+++ src/modules/medical-care/appointments/appointments.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  APPOINTMENT_REPOSITORY,
+  ATTENDANCE_REPOSITORY,
+} from 'src/core/constants';
+import { Attendance } from '../attendances/attendance.entity';
+import { Appointment } from './appointment.entity';
+import { AppointmentDto } from './dto/appointment.dto';
 
 @Injectable()
-export class AppointmentsService {}
+export class AppointmentsService {
+  constructor(
+    @Inject(APPOINTMENT_REPOSITORY)
+    private readonly appointmentRepository: typeof Appointment,
+    @Inject(ATTENDANCE_REPOSITORY)
+    private readonly attendanceRepository: typeof Attendance,
+  ) {}
+
+  async createAppointment(appointment: AppointmentDto): Promise<Appointment> {
+    const attendanceId = appointment.attendanceId;
+    const attendance = await this.attendanceRepository.findOne({
+      where: { id: attendanceId },
+    });
+
+    delete attendance.id;
+    delete attendance.statusId;
+    const newStatusId = 3;
+
+    await this.attendanceRepository.update(
+      { ...attendance, statusId: newStatusId },
+      { where: { id: attendanceId } },
+    );
+
+    return await this.appointmentRepository.create<Appointment>(appointment);
+  }
+
+  async getAppointmentById(id: number): Promise<Appointment> {
+    return await this.appointmentRepository.findOne({ where: { id } });
+  }
+}
